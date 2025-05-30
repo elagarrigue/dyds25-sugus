e39bef18d1d63c711684de2a8dcd1417cf926385
BonannoPablo <bonannopabloariel@gmail.com>
2025-05-30 10:42:31 -0300

    Creado el repositorio externo. Todavía no está conectado al resto del sistema

---
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/data/external/ExternalRepository.kt
@@ -0,0 +1,13 @@
+package edu.dyds.movies.data.external
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import edu.dyds.movies.domain.entity.RemoteMovie
+
+class ExternalRepository (val tmdbHttpClient: HttpClient){
+    public suspend fun getTMDBMovieDetails(id: Int): RemoteMovie =
+        tmdbHttpClient.get("/3/movie/$id").body()
+    public suspend fun getTMDBPopularMovies():  List<RemoteMovie> =
+        tmdbHttpClient.get("/3/discover/movie?sort_by=popularity.desc").body()
+}
