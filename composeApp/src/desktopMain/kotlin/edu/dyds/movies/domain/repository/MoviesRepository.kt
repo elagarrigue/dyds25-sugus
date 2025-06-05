c7c8db18a35cb6761d2d81c0dee03e4d0d3c49fd
BonannoPablo <bonannopabloariel@gmail.com>
2025-06-04 22:09:12 -0300

    Refactorizado GetPopularMoviesUseCase y actualizada la interfaz Repository

--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/domain/repository/MoviesRepository.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/domain/repository/MoviesRepository.kt
@@ -1,11 +1,8 @@
 package edu.dyds.movies.domain.repository
 
-import edu.dyds.movies.data.external.ExternalRepository
-import edu.dyds.movies.data.local.MoviesCache
 import edu.dyds.movies.domain.entity.Movie
-import io.ktor.client.*
 
 interface MoviesRepository {
-    suspend fun getTMDBMovieDetails(id: Int): Movie
-    suspend fun getTMDBPopularMovies():  List<Movie>
+    suspend fun getMovieDetails(id: Int): Movie?
+    suspend fun getPopularMovies():  List<Movie>
 }
--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/domain/usecase/GetPopularMoviesUseCase.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/domain/usecase/GetPopularMoviesUseCase.kt
@@ -8,21 +8,19 @@ private const val MIN_VOTE_AVERAGE = 6.0
 
 class GetPopularMoviesUseCase(private val repository: MoviesRepository) {
 
-    suspend operator fun invoke(): List<QualifiedMovie> {
-        return getPopularMovies().sortAndMap()
-    }
-
+    suspend operator fun invoke(): List<QualifiedMovie> =
+        getPopularMovies().sortByQualification().mapToQualification()
     private suspend fun getPopularMovies() =
-        repository.getTMDBPopularMovies()
+        repository.getPopularMovies()
 
-    private fun List<Movie>.sortAndMap(): List<QualifiedMovie> {
-        return this
-            .sortedByDescending { it.voteAverage }
-            .map {
-                QualifiedMovie(
-                    movie = it,
-                    isGoodMovie = it.voteAverage >= MIN_VOTE_AVERAGE
-                )
-            }
-    }
+    private fun List<Movie>.sortByQualification(): List<Movie> =
+        this.sortedByDescending { it.voteAverage }
+
+    private fun List<Movie>.mapToQualification(): List<QualifiedMovie> =
+        this.map {
+            QualifiedMovie(
+                movie = it,
+                isGoodMovie = it.voteAverage >= MIN_VOTE_AVERAGE
+            )
+        }
 }
