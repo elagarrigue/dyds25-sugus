8291d0aab0d321bc1855bcd7b2590a3fe13dc23a
Valentino <valennvillar@gmail.com>
2025-06-20 13:57:23 -0300

    Implementacion de testDetailViewModel completa

--- composeApp/src/desktopTest/kotlin/TestDetailViewModel.kt
+++ composeApp/src/desktopTest/kotlin/TestDetailViewModel.kt
@@ -1,2 +1,83 @@
+import edu.dyds.movies.domain.usecase.GetMovieDetailsUseCase
+import edu.dyds.movies.domain.usecase.GetMovieDetailsUseCaseImpl
+import edu.dyds.movies.presentation.detail.DetailViewModel
+import edu.dyds.movies.presentation.home.HomeViewModel
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+@OptIn(ExperimentalCoroutinesApi::class)
 class TestDetailViewModel {
+
+    val testDispatcher = UnconfinedTestDispatcher()
+    val testScope = CoroutineScope(testDispatcher)
+
+    private lateinit var detailViewModel: DetailViewModel
+    private lateinit var getMovieDetailViewModel: GetMovieDetailsUseCase
+
+    @BeforeTest
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+        getMovieDetailViewModel = mockk()
+        detailViewModel = DetailViewModel(getMovieDetailViewModel)
+    }
+
+    @AfterTest
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `should emit loading and success`() = runTest {
+        // Arrange
+        val fakeMovie = FakeMovieFactory.create(11, "Fake 11")
+        coEvery { getMovieDetailViewModel(11) } returns fakeMovie
+
+        lateinit var moviesUiState: DetailViewModel.MovieDetailUiState
+        testScope.launch {
+            detailViewModel.movieDetailStateFlow.collect { moviesUiState = it }
+        }
+
+        // Act
+        detailViewModel.getMovieDetail(11)
+        advanceUntilIdle()
+
+        // Assert
+        assertFalse(moviesUiState.isLoading)
+        assertEquals(fakeMovie, moviesUiState.movie)
+    }
+
+    @Test
+    fun `should emit loading and null`() = runTest {
+        // Arrange
+        coEvery { getMovieDetailViewModel(11) } returns null
+
+        lateinit var moviesUiState: DetailViewModel.MovieDetailUiState
+        testScope.launch {
+            detailViewModel.movieDetailStateFlow.collect { moviesUiState = it }
+        }
+
+        // Act
+        detailViewModel.getMovieDetail(11)
+        advanceUntilIdle()
+
+        // Assert
+        assertFalse(moviesUiState.isLoading)
+        assertEquals(null, moviesUiState.movie)
+    }
 }
